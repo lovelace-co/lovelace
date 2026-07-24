05fb9cd24b79f976753a45a0f2c7b3735f4f081a
Fredy Lievano <fredy@fredylievano.com>
2026-07-24 10:03:33 +1000

    feat: copy icon beside the ticket id in the properties panel (T-0147)

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -395,6 +395,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .manage(WatchRegistry(Mutex::new(HashMap::new())))
         .manage(WindowCounter(AtomicU32::new(2)))
         .manage(StagedUpdate(Mutex::new(None)))
